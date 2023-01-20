c9e283666d926244feb54d5112301a4b66c62667
Kade <kadeshikaal@gmail.com>
2023-01-20 20:06:30 +0200

    Added exercise controller functions

--- src/routes/exercises/exercises.controller.js
+++ src/routes/exercises/exercises.controller.js
@@ -1,6 +1,7 @@
 const db = require("../../models/index");
 const mongoose = require("mongoose");
 const Exercise = db.exercise;
+const User = db.user;
 const MuscleGroup = db.muscle_group;
 const ExerciseType = db.exercise_type;
 
@@ -10,7 +11,7 @@ exports.getConstantData = async (req, res, next) => {
         const allExerciseTypes = await ExerciseType.find();
         const allMuscleGroups = await MuscleGroup.find();
 
-        return res.send({ 
+        return res.send({
             message: "Successfully retrieved data",
             data: {
                 exercise_types: allExerciseTypes,
@@ -18,7 +19,7 @@ exports.getConstantData = async (req, res, next) => {
             }
         })
     } catch (error) {
-        return res.status(500).send({ 
+        return res.status(500).send({
             message: "Error retrieving data"
         })
     }
@@ -26,115 +27,148 @@ exports.getConstantData = async (req, res, next) => {
 
 exports.createExercise = async (req, res, next) => {
     try {
-        if(req.body.media){
+        if (req.body.media) {
             // Save media to S3 and insert into Media table
         }
-        if(req.body.personal_exercise){
-            await Exercise.create({
-                name: req.body.name,
-                description: req.body.description,
-                instruction: req.body.instruction,
-                equipment: req.body.equipment,
-                exercise_type: req.body.exercise_type,
-                muscle_groups: req.body.muscle_groups,
-                creator_id: req.userId
-            }, (err, workout) => {
-                if(err) return res.status(500).send({ 
-                    message: "Error creating a personal exercise"
-                })
-    
-                return res.send({ 
-                    message: "Successfully created a personal exercise",
-                })
-            });
-        }else{
-            await Exercise.create({
-                name: req.body.name
-            }, (err, workout) => {
-                if(err) return res.status(500).send({ 
-                    message: "Error creating an exercise"
-                })
-    
-                return res.send({ 
-                    message: "Successfully created an exercise",
-                })
-            });
-        }
+        await Exercise.create({
+            name: req.body.name,
+            description: req.body.description,
+            instruction: req.body.instruction,
+            equipment: req.body.equipment,
+            exercise_type: req.body.exercise_type,
+            muscle_groups: req.body.muscle_groups,
+            creator_id: req.userId
+        }, (err, workout) => {
+            if (err) return res.status(500).send({
+                message: "Error creating a personal exercise"
+            })
+
+            return res.send({
+                message: "Successfully created a personal exercise",
+            })
+        });
     } catch (error) {
-        return res.status(500).send({ 
+        return res.status(500).send({
             message: "Error creating an exercise"
         })
     }
 }
 
 exports.viewExercise = async (req, res, next) => {
-    console.log(req.params);
     try {
         if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(404).send({ 
-                message: `Workout with id: ${req.params.id} not found`
+            return res.status(404).send({
+                message: `Exercise with id: ${req.params.id} not found`
             })
         }
 
-        let workout = await Workout.findById(req.params.id);
-        return res.send({ 
-            message: "Successfully retrieved a workout",
-            data: workout
+        let exercise = await Exercise.findById(req.params.id)
+            .populate("exercise_type", "-__v")
+            .populate("muscle_groups", "-__v");
+
+        return res.send({
+            message: "Successfully retrieved a exercise",
+            data: exercise
         })
     } catch (error) {
-        return res.status(500).send({ 
-            message: "Error retrieving a workout"
+        return res.status(500).send({
+            message: "Error retrieving a exercise"
         })
     }
 }
+
+exports.updateExercise = async (req, res, next) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(404).send({
+                message: `Exercise with id: ${req.params.id} not found`
+            })
+        }
+        let exercise = await Exercise.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            description: req.body.description,
+            instruction: req.body.instruction,
+            equipment: req.body.equipment,
+            exercise_type: req.body.exercise_type,
+            muscle_groups: req.body.muscle_groups,
+            // creator_id: req.userId,
+            // media: req.body.media
+        }, { new: true })
+            .populate("exercise_type", "-__v")
+            .populate("muscle_groups", "-__v");
+
+        return res.send({
+            message: "Successfully updated a exercise",
+            data: exercise
+        })
+
+    } catch (error) {
+        return res.status(500).send({
+            message: "Error updating a exercise"
+        })
+    }
+}
+
 
 exports.deleteExercise = async (req, res, next) => {
     console.log(req.params.id);
     try {
         if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(404).send({ 
-                message: `Workout with id: ${req.params.id} not found`
+            return res.status(404).send({
+                message: `Exercise with id: ${req.params.id} not found`
             })
         }
 
-        let workout = await Workout.findByIdAndDelete(req.params.id);
-        return res.send({ 
-            message: "Successfully deleted workout",
-            data: workout
+        let exercise = await Exercise.findByIdAndDelete(req.params.id);
+        return res.send({
+            message: "Successfully deleted exercise",
+            data: exercise
         })
     } catch (error) {
-        return res.status(500).send({ 
-            message: "Error deleting workout"
+        console.log(error);
+        return res.status(500).send({
+            message: "Error deleting exercise"
         })
     }
 }
 
-//create a method that will return all exercises
 exports.getAllExercises = async (req, res, next) => {
+    const limit = 10;
+    const page = req?.query?.page ?? 0;
+    const search = req?.query?.search ?? '';
+    const user = req?.query?.user ?? '';
+
     try {
-        let exercises = await Exercise.find();
-        return res.send({ 
+        let user_names = await User.find({
+            first_name: {$regex: user}})
+            .select("first_name")
+            .lean();
+        
+        let exercises = await Exercise.find({
+                $or: [
+                    { name: { $regex: search } },
+                    { description: { $regex: search } },
+                ],
+                $and: [
+                    { creator_id: { $in: user_names } },
+                ]
+            })
+            .sort({ name: "asc" })
+            .limit(limit)
+            .skip(limit * page)
+            .populate("creator_id", "-__v")
+
+
+        return res.send({
             message: "Successfully retrieved all exercises",
-            data: exercises
+            data: exercises,
+            page: page,
+            limit: limit
         })
     } catch (error) {
-        return res.status(500).send({ 
+        console.log(error);
+        return res.status(500).send({
             message: "Error retrieving all exercises"
         })
-    }
-}
-
-//create a method that will return all exercises by a specific user
-exports.getAllExercisesByUser = async (req, res, next) => {
-    try {
-        let exercises = await Exercise.find({creator_id: req.userId});
-        return res.send({ 
-            message: "Successfully retrieved all exercises by user",
-            data: exercises
-        });
-    } catch (error) {
-        return res.status(500).send({ 
-            message: "Error retrieving all exercises by user"
-        });
     }
 }
--- src/routes/exercises/exercises.router.js
+++ src/routes/exercises/exercises.router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const { authJwt } = require('../../middleware/index');
-const { getConstantData } = require('./exercises.controller');
+const { getConstantData, getAllExercises, createExercise, viewExercise, updateExercise, deleteExercise } = require('./exercises.controller');
 const { ADMIN_ROLE, TRAINER_ROLE } = require('../../constants/roles');
 
 const exerciseRouter = express.Router();
 
 exerciseRouter.get('/form-data', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], getConstantData);
+exerciseRouter.post('/create', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], createExercise);
+exerciseRouter.get('/all', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], getAllExercises);
+exerciseRouter.get('/:id', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], viewExercise);
+exerciseRouter.put('/edit/:id', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], updateExercise);
+exerciseRouter.delete('/delete/:id', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], deleteExercise);
 
 module.exports = exerciseRouter;
