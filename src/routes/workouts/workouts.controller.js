83b94bd4075774d4e0ffbadf0b4ecdd3608b330a
Kade <kadeshikaal@gmail.com>
2023-01-28 21:39:23 +0200

    Updated workouts controller

--- src/routes/workouts/workouts.controller.js
+++ src/routes/workouts/workouts.controller.js
@@ -1,26 +1,83 @@
 const db = require("../../models/index");
 const mongoose = require("mongoose");
 const Workout = db.workout;
+const WorkoutExercise = db.workout_exercise;
+const User = db.user;
+
+exports.getAllWorkouts = async (req, res, next) => {
+    const limit = 10;
+    const page = req?.query?.page ?? 0;
+    const search = req?.query?.search ?? '';
+    const user = req?.query?.user ?? '';
+
+    try {
+        let user_names = await User.find({
+            first_name: {$regex: user}})
+            .select("first_name")
+            .lean();
+
+        let workouts = await Workout.find({
+                $or: [
+                    { name: { $regex: search } },
+                    { description: { $regex: search } },
+                ],
+                $and: [
+                    { creator_id: { $in: user_names } },
+                ]
+            })
+            .sort({ name: "asc" })
+            .limit(limit)
+            .skip(limit * page)
+            .populate("creator_id", "-__v");
+            
+
+        return res.send({ 
+            message: "Successfully retrieved all workouts",
+            data: workouts
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ 
+            message: "Error retrieving all workouts"
+        })
+    }
+}
 
 exports.createWorkout = async (req, res, next) => {
+    const session = await mongoose.startSession();
     try {
-        await Workout.create({
+        session.startTransaction();
+
+        const workout = await Workout.create([{
             name: req.body.name,
             description: req.body?.description,
-            instruction: req.body?.instruction,
+            type: req.body?.type,
             muscle_group: req.body?.muscle_group,
-            equipment: req.body?.equipment,
+            difficulty: req.body?.difficulty,
+            is_public: req.body?.is_public,
             creator_id: req.userId
-        }, (err, workout) => {
-            if(err) return res.status(500).send({ 
-                message: "Error creating a workout"
-            })
+        }], { session });
 
-            return res.send({ 
-                message: "Successfully created a workout",
-            })
-        });
+        for(let i = 0; i < req.body.exercises.length; i++) {
+            await WorkoutExercise.create([{
+                workout_id: workout[0]._id,
+                exercise_id: req.body.exercises[i].exercise_id,
+                sets: req.body.exercises[i].sets,
+                reps: req.body.exercises[i].reps,
+                duration: req.body.exercises[i].duration,
+                distance: req.body.exercises[i].distance,
+            }], { session });
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+        
+        return res.send({ 
+            message: "Successfully created a workout",
+        })
     } catch (error) {
+        await session.abortTransaction();
+        await session.endSession();
         return res.status(500).send({ 
             message: "Error creating a workout"
         })
@@ -35,15 +92,64 @@ exports.viewWorkout = async (req, res, next) => {
                 message: `Workout with id: ${req.params.id} not found`
             })
         }
-
+        // TODO: change the workout model to include the exercises
         let workout = await Workout.findById(req.params.id);
+        let workoutExercises = await WorkoutExercise.find().where('workout_id').equals(req.params.id);
+        workout["exercises"] = workoutExercises;
+        let newWorkout = workout.toObject();
+        newWorkout.exercises = workoutExercises;
+
         return res.send({ 
             message: "Successfully retrieved a workout",
-            data: workout
+            data: newWorkout
         })
     } catch (error) {
         return res.status(500).send({ 
             message: "Error retrieving a workout"
+        })
+    }
+}
+
+exports.updateWorkout = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    try {
+        session.startTransaction();
+        console.log(req.body);
+        const workout = await Workout.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            description: req.body?.description,
+            type: req.body?.type,
+            muscle_group: req.body?.muscle_group,
+            difficulty: req.body?.difficulty,
+            is_public: req.body?.is_public
+        }, { session });
+
+        // Find and effective way to update the workout exercises
+        await WorkoutExercise.deleteMany({ workout_id: req.params.id })
+
+        for(let i = 0; i < req.body.exercises.length; i++) {
+            await WorkoutExercise.create([{
+                workout_id: req.params.id, 
+                exercise_id: req.body.exercises[i].exercise_id,
+                sets: req.body.exercises[i].sets,
+                reps: req.body.exercises[i].reps,
+                duration: req.body.exercises[i].duration,
+                distance: req.body.exercises[i].distance,
+            }], { session });
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+        
+        return res.send({ 
+            message: "Successfully updated workout",
+        })
+    } catch (error) {
+        console.log(error);
+        await session.abortTransaction();
+        await session.endSession();
+        return res.status(500).send({ 
+            message: "Error updating workout"
         })
     }
 }
@@ -56,14 +162,13 @@ exports.deleteWorkout = async (req, res, next) => {
                 message: `Workout with id: ${req.params.id} not found`
             })
         }
-
-        let workout = await Workout.findByIdAndDelete(req.params.id);
+        await WorkoutExercise.deleteMany({ workout_id: req.params.id });
+        await Workout.findByIdAndDelete(req.params.id);
         return res.send({ 
-            message: "Successfully deleted workout",
-            data: workout
+            message: "Successfully deleted workout"
         })
     } catch (error) {
-        return res.status(500).send({ 
+        return res.status(500).send({
             message: "Error deleting workout"
         })
     }
--- src/routes/workouts/workouts.router.js
+++ src/routes/workouts/workouts.router.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const { authJwt } = require('../../middleware/index');
-const { createWorkout, viewWorkout, deleteWorkout } = require('./workouts.controller');
+const { createWorkout, viewWorkout, deleteWorkout, updateWorkout, getAllWorkouts } = require('./workouts.controller');
 const { ADMIN_ROLE, TRAINER_ROLE } = require('../../constants/roles');
 
 const workoutRouter = express.Router();
 
-workoutRouter.post('/create', [authJwt.verifyToken([TRAINER_ROLE.code])], createWorkout);
-workoutRouter.get('/view/:id', viewWorkout);
-workoutRouter.get('/delete/:id', deleteWorkout);
+workoutRouter.post('/create', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], createWorkout);
+workoutRouter.get('/all', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], getAllWorkouts);
+workoutRouter.get('/:id', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], viewWorkout);
+workoutRouter.put('/edit/:id', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], updateWorkout);
+workoutRouter.delete('/delete/:id', [authJwt.verifyToken([TRAINER_ROLE.code, ADMIN_ROLE.code])], deleteWorkout);
 
 module.exports = workoutRouter;
